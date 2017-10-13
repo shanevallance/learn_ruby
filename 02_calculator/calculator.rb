aeab06b843d5e6b8453cf1008379319b99019009
shanevallance <vallance.shane@gmail.com>
2017-10-13 07:24:27 -0400

    02 and 03 successful rspec

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,14 @@
 #write your code here
+def add(num1, num2)
+  num1 + num2
+end
+def subtract(num1, num2)
+  num1 - num2
+end
+def sum(array)
+  sum = 0
+  array.each do |n|
+    sum += n
+  end
+  sum
+end  
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+def echo simonsay
+  simonsay
+end
+def shout simonshout
+  simonshout.upcase
+end
+def repeat simonrepeat, times = 2
+  repeat = ""
+  (1..times).each do |n|
+    repeat.concat("#{simonrepeat}")
+    if n < times
+      repeat += " "
+    end
+  end
+  repeat
+end
+def start_of_word(word, length)
+  word[0...length]
+end
+def first_word(sentence)
+  words = sentence.split(/[^[[:word:]]']+/)
+  words[0]
+end
+def titleize(input)
+  words = input.split(/[^[[:word:]]']+/)
+  littlewords = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "or",  "nor", "over"]
+  words.each_index do |i|
+    if !littlewords.include?(words[i]) || i = 0
+      words[i].capitalize!
+    end
+  end
+words.join(" ")
+end
