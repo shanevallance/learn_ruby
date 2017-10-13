37725c0ddb25b9eecf5ccdc27857f436d8ab9958
shanevallance <vallance.shane@gmail.com>
2017-10-13 10:07:15 -0400

    04 has no more errors

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
 #write your code here
+def translate(input)
+  words = input.downcase.split(/[^[[:word:]]']+/)
+  words.each_index do |i|
+    #if first letter is not a vowel
+    #I'm positive there is a more concise way to do this
+    if !%w[a e i o u].include?(words[i][0])
+      #if second letter is not a vowel or if there are any known phenome in the first 3
+      if !%w[a e i o u].include?(words[i][1]) || %w[sch qu].any? {|phenome| words[i][0..2].include?(phenome)}
+        #if the third letter is consonant or if the last 2 are any 2 letter phenomes
+        if !%w[a e i o u].include?(words[i][2]) || words[i][0..2].include?("sch") || words[i][1..2].include?("qu")
+          words[i] = words[i].concat("#{words[i][0..2]}ay")[3..-1]
+        else
+          words[i] = words[i].concat("#{words[i][0..1]}ay")[2..-1]
+        end
+      #if second letter is vowel
+      else
+        words[i] = words[i].concat("#{words[i][0]}ay")[1..-1]
+      end
+    #if first letter is a vowel
+    else
+      words[i].concat('ay')
+    end
+  end
+  words.join(" ")
+end
