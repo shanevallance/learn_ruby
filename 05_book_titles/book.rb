f8ec7c82ba6b9ed5838c9841069a07e1d140c03f
shanevallance <vallance.shane@gmail.com>
2017-10-13 14:14:14 -0400

    05 returns no errors rspec

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
 # write your code here
+  def title= input
+    @title = titleize(input)
+  end
+  def titleize(input)
+    words = input.split(/[^[[:word:]]']+/)
+    littlewords = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "or",  "nor", "over"]
+    words.each_index do |i|
+      if !littlewords.include?(words[i]) || i = 0
+        words[i].capitalize!
+      end
+    end
+    words.join(" ")
+  end
+  def title
+    @title
+  end
+
 end
