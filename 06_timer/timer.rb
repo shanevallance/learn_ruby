60f3a160fe439c7d531ed8ca284ded0d150c0196
shanevallance <vallance.shane@gmail.com>
2017-10-16 14:07:26 -0400

    06 no errors

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
 class Timer
   #write your code here
+  def initialize seconds = 0
+    @seconds = seconds
+  end
+  def seconds= input
+    @seconds = input
+  end
+  def seconds
+    @seconds
+  end
+  def time_string
+    #Get hours
+    hours = @seconds/3600
+    #get minutes
+    minutes = @seconds/60 - (hours * 60)
+    #get seconds
+    seconds = @seconds%60
+    "#{hours.to_s.rjust(2,"0")}:#{minutes.to_s.rjust(2,"0")}:#{seconds.to_s.rjust(2,"0")}"
+  end
+
 end
